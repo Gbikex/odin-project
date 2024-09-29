0001583929b68080318629940323087d4baa7f1a
Gbikex <csatgabor@gmail.com>
2024-09-29 10:53:09 +0200

    Player move added with handling the input values

--- JS-Projects/r-p-sc.js
+++ JS-Projects/r-p-sc.js
@@ -1,14 +1,27 @@
 
-let userInput = 'Scissor';
-let ComputerMove = getComputerMove();
+  let userInput = playerMove();
+  let ComputerMove = getComputerMove();
 
 /* Logs to test the values for development */
-console.log(userInput);
-console.log(ComputerMove);
+  console.log(userInput);
+  console.log(ComputerMove);
 
+/* Player move => input value, accepts only rock/paper/scissors case sensitivity does not matter, handled in the function*/
+  function playerMove() {
+
+    let playerMove = prompt('To start the game please type in : rock, paper or scissor!');
+
+    playerMove = playerMove.toLowerCase();
+    playerMove = playerMove.charAt(0).toUpperCase() + playerMove.slice(1);
+    
+    if (playerMove === 'Rock' || playerMove === 'Paper' || playerMove === 'Scissor') {
+      return playerMove
+    } else alert('Please type in rock,paper or scissor to start the game!');
+    location.reload();
+  }
 
 /* Function generates random moves via Math.random function */
-function getComputerMove() {
+  function getComputerMove() {
 
     const randomNumber = Math.random();
     let computerMove;
@@ -33,25 +46,25 @@ function getComputerMove() {
     if (userInput === 'Rock') {
       if (ComputerMove === 'Rock') {
         console.log(`Player choose ${userInput} and Computer choose ${ComputerMove}, result: Draw!`);
-      } else if(ComputerMove === 'Paper') {
+      }else if (ComputerMove === 'Paper') {
         console.log(`Player choose ${userInput} and Computer choose ${ComputerMove}, result: Computer Win!`);
-      } else if(ComputerMove === 'Scissor') {
+      }else if (ComputerMove === 'Scissor') {
         console.log(`Player choose ${userInput} and Computer choose ${ComputerMove}, result: Player Win!`);
       }
       }else if (userInput === 'Paper') {
         if (ComputerMove === 'Rock') {
           console.log(`Player choose ${userInput} and Computer choose ${ComputerMove}, result: Player Win!`);
-        } else if(ComputerMove === 'Paper') {
+        }else if (ComputerMove === 'Paper') {
           console.log(`Player choose ${userInput} and Computer choose ${ComputerMove}, result: Draw!`);
-        } else if(ComputerMove === 'Scissor') {
+        }else if (ComputerMove === 'Scissor') {
           console.log(`Player choose ${userInput} and Computer choose ${ComputerMove}, result: Computer Win!`);
         }
-      } else if (userInput === 'Scissor') {
+      }else if (userInput === 'Scissor') {
         if (ComputerMove === 'Rock') {
           console.log(`Player choose ${userInput} and Computer choose ${ComputerMove}, result: Computer Win!`);
-        } else if(ComputerMove === 'Paper') {
+        }else if (ComputerMove === 'Paper') {
           console.log(`Player choose ${userInput} and Computer choose ${ComputerMove}, result: Player Win!`);
-        } else if(ComputerMove === 'Scissor') {
+        }else if (ComputerMove === 'Scissor') {
           console.log(`Player choose ${userInput} and Computer choose ${ComputerMove}, result: Draw!`);
         }
       }
