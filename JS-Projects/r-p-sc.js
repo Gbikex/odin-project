c316ccd535795fbffc29f7ec7941e0ff0542e1a4
Gbikex <csatgabor@gmail.com>
2024-09-29 08:13:15 +0200

    Adding return values and base logic for playRPSCGame funion (only rock)

--- JS-Projects/r-p-sc.js
+++ JS-Projects/r-p-sc.js
@@ -1,26 +1,43 @@
-console.log('Hello');
 
+/* Function generates random moves via Math.random function */
 
+  let userInput = 'Rock';
 
 function getComputerMove() {
 
-  const randomNumber = Math.random();
-  let computerMove;
+    const randomNumber = Math.random();
+    let computerMove;
+    /*console.log(randomNumber);*/
 
-  console.log(randomNumber);
+    if (randomNumber >= 0 && randomNumber <= 0.3 ) {
+      return computerMove = 'Rock';
+      console.log(computerMove);
+    }
+    else if (randomNumber > 0.3 && randomNumber <= 0.6) {
+      return computerMove = 'Paper';
+      console.log(computerMove);
+    } 
+    else if (randomNumber > 0.6 && randomNumber <= 1) {
+      return computerMove = 'Scissors';
+      console.log(computerMove);
+    } 
+  };
 
-  if (randomNumber >= 0 && randomNumber <= 0.3 ) {
-    computerMove = 'Rock';
-    console.log(computerMove)
+  let ComputerMove = getComputerMove();
+
+  console.log(ComputerMove);
+
+
+  function playRPSCGame(){
+    if (userInput === 'Rock') {
+      if (ComputerMove === 'Rock' ) {
+        console.log(`Player choose ${userInput} and Computer choose ${ComputerMove}, result: draw!`);
+      }
+      else ('Under development');
+    }
+
   }
-  else if (randomNumber > 0.3 && randomNumber <= 0.6) {
-    computerMove = 'Paper';
-    console.log(computerMove)
-  } 
-  else if (randomNumber > 0.6 && randomNumber <= 1) {
-    computerMove = 'Scissors';
-    console.log(computerMove)
-  } 
-};
 
-getComputerMove();
+  playRPSCGame();
+
+
